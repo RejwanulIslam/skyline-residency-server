0ea642d3f89c0868128cc22b0220e073144b4e19
Rejwanul Islam <RejwanulIslam@users.noreply.github.com>
2025-08-10 18:43:16 +0600

    creact jwt and veryfy token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const app = express()
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
 
 //midileWare
@@ -24,20 +25,39 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
 
-       const cupponCollection=client.db("Skyline-Residency").collection("cuppon")
-       const apartmentCollection=client.db("Skyline-Residency").collection("apartment")
-          
+        const cupponCollection = client.db("Skyline-Residency").collection("cuppon")
+        const apartmentCollection = client.db("Skyline-Residency").collection("apartment")
 
+        //jwt
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '365d' })
+            res.send({ token })
+        })
 
-       app.get('/cuppon',async(req,res)=>{
-        const result=await cupponCollection.find().toArray()
-        res.send(result)
-       })
+        const veryfyToken = (req, res, next) => {
+            if (!req.headers.Authorization) {
+                return res.status(401).send({ message: 'forbidding access' })
+            }
+            const token = req.headers.Authorization.split(' ')[1]
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+                if (err) {
+                    return res.status(401).send({ message: 'forbidding access' })
 
-       app.get('/apartment',async(req,res)=>{
-        const result=await apartmentCollection.find().toArray()
-        res.send(result)
-       })
+                }
+                req.decoded = decoded
+                next()
+            })
+        }
+        app.get('/cuppon', async (req, res) => {
+            const result = await cupponCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/apartment', async (req, res) => {
+            const result = await apartmentCollection.find().toArray()
+            res.send(result)
+        })
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
@@ -45,13 +65,13 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-       // await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('server is running')
 })
 
